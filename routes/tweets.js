03edeec12834b4a0c4b59cc6f3487c83dcf5c805
Alec Venida <alecvenida@gmail.com>
2022-06-03 11:15:53 -0700

    before lunch call TA why 404

--- routes/tweets.js
+++ routes/tweets.js
@@ -4,6 +4,8 @@ const db = require("../db/models");
 const { Tweet } = db;
 const router = express.Router();
 
+const { check, validationResult } = require('express-validator');
+
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 const tweetNotFoundError = (id) => {
   const err = Error(`Tweet with id of ${id} could not be found`);
@@ -35,6 +37,45 @@ router.get(
   })
 )
 
+
+
+// tweet validation: you're great, tweets!
+const validateTweets = [
+  check("message")
+  .exists({checkFalsy: true})
+  .withMessage("Message Can't Be Empty"),
+  check("message")
+  .isLength({max: 280})
+  .withMessage("Tweet can't be longer than 280 characters")
+]
+
+
+
+
+//validation middleware
+const handleValidationErrors = (req, res, next) => {
+  const validationErrors = validationResult(req);
+  //ToDo
+
+  if (!validationErrors.isEmpty()) {  //if it's not empty
+    const errorMessages = validationErrors.array().map((error) => error.msg);  //creates array of all the error messages
+
+      const errorObject = Error('Bad Request.');
+      errorObject.errors = errorMessages
+      errorObject.status = 400
+      errorObject.title = "Bad Request."
+      return next(errorObject)
+  }
+  next()
+}
+
+
+router.post("/tweets", validateTweets, handleValidationErrors, asyncHandler(async(req, res) => {        // create a new tweet
+  const { tweet } = req.body;
+  const newTweet = await Tweet.create({ tweet });
+  res.status(201).json({ newTweet })
+})
+);
 
 
 
